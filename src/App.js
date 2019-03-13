24ccc9aca27687ddebad8dd4babb030dc0164550
Elizabeth Le <elizabethlemk@yahoo.com>
2019-03-13 10:14:59 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import MasterHog from './MasterHog'
 import './App.css'
 
 export default class App extends Component {
-  
+
   render() {
     return (
       <div className="App">
-        <MasterHog />
+        <MasterHog weight={2.54} />
       </div>
     )
   }
